e4cd01659bcc3285f2d3cde9dc08e2d6135950b4
arielalmeida <arielcessar8@gmail.com>
2020-10-21 22:49:30 -0300

    cambios con bootstrap trabajo 07

---
+++ trabajo_08_js/ejercicioParte2.js
@@ -0,0 +1,18 @@
+function cookies(){
+    var nuevoDato = nuevoDato();
+    nuevoDato(nuevoDato.getTime() + (exdays *24*60*60*3000));
+    var expira = "expira" + nuevoDato.toUTCString();
+    document.cookie
+
+}
+
+function cambioPagina(){
+   let cambiarPagina = window.confirm("Estas a punto de entrar a otro link. ¿Estas seguro?"); 
+        if(cambiarPagina==true){
+           window.location.assign("https://www.google.com.ar");
+        }else{
+            if(cambiarPagina==false){
+                window.location.assign("file:///C:/COMit/workspace/js/trabajo_08_js/ejercicioParte2.html");
+            }
+        }        
+    }
--- trabajo_08_js/trabajo_08_js.js
+++ trabajo_08_js/trabajo_08_js.js
@@ -1,28 +1,36 @@
-function validarNombre(valor){
-  let dato1 = document.getElementById('nombre').innerHTML;
+/*function validarNombre(valor){
     if(valor.length<1){
         alert("No ingresaste tu Nombre");
 
     }      
   }
-function validarEdad(edad){
-    let edadIngresada = document.getElementById('edad').innerHTML;
+function validarEdad(edad){ 
         if(edad<18){
             alert("Tu edad no esta permitida");
 
     }      
-  }
+  }*/ 
   
 function cambiaOpciones(){
-  let trabajador_1 = ["monotributista","Relacion_de_dependencia","Informal"];
-  let estudiante_1 = ["secundario","terciario","universitario"];
+  
   let trabajador = document.getElementById('trabajador').value;
   let estudiante = document.getElementById('estudiante').value;
- 
-    if(trabajador = 0){
-      document.getElementById('trabajador_1').value = trabajador_1;
+        if(trabajador==0){
+       let n1= document.getElementByName('1').innerHTML = "-----";
+       let n2= document.getElementByName('2').innerHTML = "monotributista";
+       let n3= document.getElementByName('3').innerHTML = "relacion de Dependencia";
+       let n4= document.getElementByName('4').innerHTML = "informal";
+        }else{
+          if(estudiante<1){
+            let n1= document.getElementById('cam0').innerHTML = "--------";
+            let n2= document.getElementById('cam1').innerHTML = "secundario";
+            let n3= document.getElementById('cam2').innerHTML = "terciario";
+            let n4= document.getElementById('cam3').innerHTML = "universitario";
+          }
+        }
+      }
 
-    }
+         
 
+          
 
-}
