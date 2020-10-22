bc5b31d8687a97677b5f4de9cee6853561920126
arielalmeida <arielcessar8@gmail.com>
2020-10-22 00:13:52 -0300

    cambios en trabajo-08

--- trabajo_08_js/trabajo_08_js.js
+++ trabajo_08_js/trabajo_08_js.js
@@ -1,4 +1,4 @@
-/*function validarNombre(valor){
+function validarNombre(valor){
     if(valor.length<1){
         alert("No ingresaste tu Nombre");
 
@@ -9,28 +9,16 @@ function validarEdad(edad){
             alert("Tu edad no esta permitida");
 
     }      
-  }*/ 
+  }
   
 function cambiaOpciones(){
   
   let trabajador = document.getElementById('trabajador').value;
   let estudiante = document.getElementById('estudiante').value;
         if(trabajador==0){
-       let n1= document.getElementByName('1').innerHTML = "-----";
-       let n2= document.getElementByName('2').innerHTML = "monotributista";
-       let n3= document.getElementByName('3').innerHTML = "relacion de Dependencia";
-       let n4= document.getElementByName('4').innerHTML = "informal";
-        }else{
-          if(estudiante<1){
-            let n1= document.getElementById('cam0').innerHTML = "--------";
-            let n2= document.getElementById('cam1').innerHTML = "secundario";
-            let n3= document.getElementById('cam2').innerHTML = "terciario";
-            let n4= document.getElementById('cam3').innerHTML = "universitario";
-          }
+       document.getElementById('1').innerHTML = "-----";
+       document.getElementById('2').innerHTML = "monotributista";
+       document.getElementById('3').innerHTML = "relacion de Dependencia";
+       document.getElementById('4').innerHTML = "informal";
         }
-      }
-
-         
-
-          
-
+     }
